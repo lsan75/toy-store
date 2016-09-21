edceb71ef969864475f754f96c5177e2c4b11897
Laurent Sanchez <laurent.sanchez-ext@amundi.com>
2016-09-21 14:55:59 +0200

    modify package

--- www/js/app.routes.ts
+++ www/js/app.routes.ts
@@ -25,4 +25,9 @@ const appRoutes: Routes = [
   }
 ]
 
-export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes, { useHash: true })
+export const routing: ModuleWithProviders = RouterModule.forRoot(
+  appRoutes,
+  {
+    useHash: true
+  }
+)
